76a733d1dc1f5234e642a0cd71b8d82353cb4e7e
Emmanuel <emmanuelcoryl@gmail.com>
2022-12-14 00:31:02 +0300

    4

---
+++ not-property.js
@@ -0,0 +1,20 @@
+let temp = parseInt(prompt('please input temprature'));
+let sunny = prompt('is it sunny: true or false?');
+
+if (!(temp >= 0)) {
+ console.log('it is cold') 
+ alert('it is cold')
+}
+else{
+  console.log('it is warm')
+alert('it is warm')
+}
+
+if(!sunny){
+  alert('its cold outside')
+  console.log('its cold outside')
+}
+else{
+  alert('its warm outside')
+  console.log('its warm outside')
+}
